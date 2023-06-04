f22b928e32d80f9c9602b419b7dd7f964f142c8a
Leul <leulwebsdev@gmail.com>
2023-06-04 11:44:41 +0300

    latest renew

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -16,12 +16,12 @@
     const emit = defineEmits('toggle')
 
     const images= [
-        "url('../assets/2.jpg')",
-        "url('../assets/1.jpg')",
-        "url('../assets/3.jpg')",
-        "url('../assets/4.jpg')",
-        "url('../assets/5.jpg')",
-        "url('../assets/cars6.jpg')",
+        "url('./src/assets/3.jpg')",
+        "url('./src/assets/2.jpg')",
+        "url('./src/assets/1.jpg')",
+        "url('./src/assets/4.jpg')",
+        "url('./src/assets/5.jpg')",
+        "url('./src/assets/cars6.jpg')",
     ]
 
     const keyWords = [
@@ -136,52 +136,57 @@
         </div>            
         </div>
             
+
+
+
+
+        <main class=" sm:hidden  h-screen text-white relative inset-0  right-0 w-screen overflow-x-hidden">
+            <video class="object-cover md:hidden h-screen" autoplay muted loop id="myVideo">
+                <source src="../assets/phoneHero.mp4" type="video/mp4">
+              </video>
+    
+              <div v-if="showNav"  class="sm:hidden z-10 w-[50vw] h-screen absolute top-0 right-0 bg-black/80 px-4 overflow-hidden ">
+                <div class="flex flex-col items-start h-full justify-start py-4">
+                    <div  @click="emit('toggle')"  class="block md:hidden self-end cursor-pointer my-10">
+                        <i class="fa-regular fa-x"></i>
+                    </div>
+                    <div>
+                        <img src="../assets/wlogo.png" alt="">
+                    </div>
+                    <div>
+                        <ul class="flex flex-col space-y-5 mt-5">
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#products">Products</a></li>
+                            <li><a href="#feature">Feature</a></li>
+                            <li><a href="#about">About us</a></li>
+                            <li> <a href="#contact">Contact us</a> </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    
+              <section class="bg-black/80 absolute w-screen h-full flex flex-col items-center justify-center top-0 right-0">
+                <div class="w-11/12 mx-auto">
+                    <p class="hero text-4xl font-bold text-indigo-600 ">ROBLAS supply chain management</p>
+                <p class="mt-4 hero text-justify ">
+                   Discover Roblas Supply Chain Management Pty Ltd, your trusted partner for a diverse range of exceptional products. From tiles to cars, agriculture tools to security systems, we deliver quality and value. With expertise in agro food commodities, we ensure constant supply and competitive prices. Plant the seeds of success with us today.
+                    <!-- <strong id="hero-text"  class="hero-text text-2xl font-bold text-blue-600 uppercase">
+                        {{ keyWords[word] }}
+                    </strong> -->
+                </p>
+    
+                <div class="w-44 bg-indigo-600 text-lg font-semibold rounded-md mt-3 flex justify-center items-center py-3 space-x-4">
+                    <p><a href="#contact">Let's talk</a></p>
+                    <i class="fa-solid fa-arrow-right text-lg"></i>
+                </div>
+                </div>
+              </section>
+             
+        </main>
     </main> 
 
 
-    <main class=" sm:hidden  h-screen text-white relative inset-0  right-0 w-screen overflow-x-hidden">
-        <video class="object-cover md:hidden h-screen" autoplay muted loop id="myVideo">
-            <source src="../assets/phoneHero.mp4" type="video/mp4">
-          </video>
-
-          <div v-if="showNav"  class="sm:hidden z-10 w-[50vw] h-screen absolute top-0 right-0 bg-black/80 px-4 overflow-hidden ">
-            <div class="flex flex-col items-start h-full justify-start py-4">
-                <div  @click="emit('toggle')"  class="block md:hidden self-end cursor-pointer my-10">
-                    <i class="fa-regular fa-x"></i>
-                </div>
-                <div>
-                    <img src="../assets/wlogo.png" alt="">
-                </div>
-                <div>
-                    <ul class="flex flex-col space-y-5 mt-5">
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#products">Products</a></li>
-                        <li><a href="#feature">Feature</a></li>
-                        <li><a href="#about">About us</a></li>
-                        <li> <a href="#contact">Contact us</a> </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-
-          <section class="bg-black/80 absolute w-screen h-full flex flex-col items-center justify-center top-0 right-0">
-            <div class="w-11/12 mx-auto">
-                <p class="hero text-4xl font-bold text-indigo-600 ">ROBLAS supply chain management</p>
-            <p class="mt-4 hero text-justify ">
-               Discover Roblas Supply Chain Management Pty Ltd, your trusted partner for a diverse range of exceptional products. From tiles to cars, agriculture tools to security systems, we deliver quality and value. With expertise in agro food commodities, we ensure constant supply and competitive prices. Plant the seeds of success with us today.
-                <!-- <strong id="hero-text"  class="hero-text text-2xl font-bold text-blue-600 uppercase">
-                    {{ keyWords[word] }}
-                </strong> -->
-            </p>
-
-            <div class="w-44 bg-indigo-600 text-lg font-semibold rounded-md mt-3 flex justify-center items-center py-3 space-x-4">
-                <p><a href="#contact">Let's talk</a></p>
-                <i class="fa-solid fa-arrow-right text-lg"></i>
-            </div>
-            </div>
-          </section>
-         
-    </main>
+    
 </template>
 
 
--- src/data/products.js
+++ src/data/products.js
@@ -1,71 +1,71 @@
 let productsList = [
     {
         title: "tiles",
-        img: "../assets/rtile.jpg",
+        img: "./src/assets/rtile.jpg",
         description: "Experience the epitome of elegance with our imported tiles. Meticulously crafted, each tile exudes beauty, durability, and sophistication. Transform your space into a captivating masterpiece with our exquisite collection, redefining luxury one tile at a time."
     },
 
     {
         title: "Building Materials",
-        img: "../assets/building.jpg",
+        img: "./src/assets/building.jpg",
         description: "Experience extraordinary elegance with our imported building materials. Crafted with meticulous care, they redefine excellence, elevating your spaces to new heights. Discover the perfect blend of style and durability, creating remarkable environments that leave a lasting impression."
     },
 
     {
         title: "Toilet Accessories",
-        img: "../assets/bathroom.jpg",
+        img: "./src/assets/bathroom.jpg",
         description: "Enhance your bathroom with our stylish toilet accessories. Meticulously crafted for convenience and elegance, they elevate your space effortlessly. Experience sophistication and luxury in every detail."
     },
 
     {
         title: "Home Accessories",
-        img: "../assets/rhome.jpg",
+        img: "./src/assets/rhome.jpg",
         description: "Unleash the true potential of your home with our captivating accessories. From stunning décor pieces to functional essentials, each item is meticulously crafted to infuse your space with style and personality."
     },
 
     {
         title: "cars",
-        img: "../assets/6.jpg",
+        img: "./src/assets/6.jpg",
         description: "Upgrade your ride with our premium car accessories. Meticulously crafted for style and functionality, they elevate your driving experience. From sleek exterior enhancements to luxurious interior comforts, enjoy automotive excellence with our exceptional collection."
     },
 
     {
         title: "3 wheel automotive",
-        img: "../assets/r3wheels.jpg",
+        img: "./src/assets/r3wheels.jpg",
         description: "Unleash the thrill of three-wheel vehicles with our exceptional accessories. Meticulously designed for performance and style, each accessory enhances your ride. From sleek exterior enhancements to cutting-edge features, elevate your three-wheel vehicle to new heights with our premium collection."
     },
 
     {
         title: "Agriculture tools",
-        img: "../assets/ragriculture.jpg",
+        img: "./src/assets/ragriculture.jpg",
         description: "Optimize your farming with our advanced agriculture tools. Meticulously designed for efficiency and durability, they enhance productivity in the field. From innovative planting solutions to cutting-edge harvesting equipment, elevate your agricultural practices with our exceptional tools."
     },
     {
         title: "Raw material for industries and manfacturing companies",
-        img: "../assets/industry.jpg",
+        img: "./src/assets/industry.jpg",
         description: "Unleash Industrial Potential: Unlock the power of your operations with our premium-grade raw materials, fueling innovation and driving success."
     },
     {
         title: "Kids Clothing & Toys",
-        img: "../assets/toy.jpg",
+        img: "./src/assets/toy.jpg",
         description: "Spark joy and imagination with our delightful collection of kids' clothes and toys. From charming outfits that capture their personalities to captivating toys that inspire endless fun, let your children's world come alive with our exceptional selection."
     },
 
     {
         title: "Spare parts",
-        img: "./assets/spare.jpg",
+        img: "./src/assets/spare.jpg",
         description: "Ensure seamless operations with our high-quality spare parts. Meticulously crafted for reliability, they keep your machinery running smoothly. Trust in our exceptional selection to minimize downtime and maximize productivity in your industry or company."
     },
 
     {
         title: "Security system accessories",
-        img: "../assets/security.jpg",
+        img: "./src/assets/security.jpg",
         description: "Experience peace of mind with our advanced security systems. Meticulously designed for robust protection, they safeguard your premises. Trust in our exceptional solutions to ensure the safety and security of your property and assets."
     },
 
     {
         title: "Agro food comodities",
-        img: "../assets/ragro_food.jpg",
+        img: "./src/assets/ragro_food.jpg",
         description: "Delight in the finest agro food commodities. Meticulously sourced for quality, they redefine culinary experiences. Elevate your dishes with our exceptional selection, crafted to inspire gastronomic excellence."
     },
 ];
